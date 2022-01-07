c87614a9aec4ee960343fe266754e09e64c1b74e
Kaan Serin <kaanserin113@gmail.com>
2022-01-07 22:59:20 +0200

    Update skeleton

--- src/components/CarCustomizerSkeleton/CarCustomizerSkeleton.js
+++ src/components/CarCustomizerSkeleton/CarCustomizerSkeleton.js
@@ -4,13 +4,9 @@ import 'react-loading-skeleton/dist/skeleton.css'
 const CarCustomizerSkeleton = () => {
     return (
         <div>
-            <div style={{width: '100px'} }>
-                <Skeleton height='1.5rem' />
-            </div>
+            <div style={{textAlign:'start', fontWeight: 'bold', marginBottom: '0.5rem'}}>CKONFIG 5.1</div>
 
-            <div style={{width: '80px',  margin: '0.5rem 0'} }>
-                <Skeleton height='1rem' />
-            </div>
+            <div style={{textAlign:'start', marginBottom: '0.25rem'}}>Models</div>
             <div style={{display: 'flex', marginBottom: '1rem'}}>
                 <Skeleton height='2rem' style={{width: '100px', marginRight: '1rem'}}/>
                 <Skeleton height='2rem' style={{width: '100px', marginRight: '1rem'}}/>
@@ -18,9 +14,7 @@ const CarCustomizerSkeleton = () => {
                 <Skeleton height='2rem' style={{width: '100px', marginRight: '1rem'}}/>
             </div>
 
-            <div style={{width: '80px',  margin: '0.5rem 0'} }>
-                <Skeleton height='1rem' />
-            </div>
+            <div style={{textAlign:'start', marginBottom: '0.25rem'}}>Engines</div>
             <div style={{display: 'flex', marginBottom: '1rem'}}>
                 <Skeleton height='2rem' style={{width: '100px', marginRight: '1rem'}}/>
                 <Skeleton height='2rem' style={{width: '100px', marginRight: '1rem'}}/>
@@ -28,17 +22,13 @@ const CarCustomizerSkeleton = () => {
                 <Skeleton height='2rem' style={{width: '100px', marginRight: '1rem'}}/>
             </div>
 
-            <div style={{width: '80px',  margin: '0.5rem 0'} }>
-                <Skeleton height='1rem' />
-            </div>
+            <div style={{textAlign:'start', marginBottom: '0.25rem'}}>Gearbox</div>
             <div style={{display: 'flex', marginBottom: '1rem'}}>
                 <Skeleton height='2rem' style={{width: '100px', marginRight: '1rem'}}/>
                 <Skeleton height='2rem' style={{width: '100px', marginRight: '1rem'}}/>
             </div>
 
-            <div style={{width: '80px',  margin: '0.5rem 0'} }>
-                <Skeleton height='1rem' />
-            </div>
+            <div style={{textAlign:'start', marginBottom: '0.25rem'}}>Colors</div>
             <div style={{display: 'flex', marginBottom: '1rem'}}>
                 <Skeleton height='2.5rem' style={{width: '40px', marginRight: '1rem'}}/>
                 <Skeleton height='2.5rem' style={{width: '40px', marginRight: '1rem'}}/>
